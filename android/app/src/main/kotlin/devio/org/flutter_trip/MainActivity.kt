3605b718a1534e6a8cd4449831ace4098a880060
wz <291171110@qq.com>
2020-01-07 19:07:40 +0800

    项目初始化；BottomNavigator。

---
+++ android/app/src/main/kotlin/devio/org/flutter_trip/MainActivity.kt
@@ -0,0 +1,12 @@
+package devio.org.flutter_trip
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
