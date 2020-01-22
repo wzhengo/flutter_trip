50a233d9ba47cba63ba7b549e4785828f38a1daf
wz <291171110@qq.com>
2020-01-22 11:28:43 +0800

    搜索页ListItem富文本展示

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -4,6 +4,21 @@ import 'package:flutter_trip/model/seach_model.dart';
 import 'package:flutter_trip/widget/search_bar.dart';
 import 'package:flutter_trip/widget/webview.dart';
 
+const TYPES = [
+  'channelgroup',
+  'gs',
+  'plane',
+  'train',
+  'cruise',
+  'district',
+  'food',
+  'hotel',
+  'huodong',
+  'shop',
+  'sight',
+  'ticket',
+  'travelgroup'
+];
 const URL =
     'https://m.ctrip.com/restapi/h5api/searchapp/search?source=mobileweb&action=autocomplete&contentType=json&keyword=';
 
@@ -112,16 +127,24 @@ class _SearchPageState extends State<SearchPage> {
             border: Border(bottom: BorderSide(width: 0.3, color: Colors.grey))),
         child: Row(
           children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(1),
+              child: Image(
+                height: 26,
+                width: 26,
+                image: AssetImage(_typeImage(item.type)),
+              ),
+            ),
             Column(
               children: <Widget>[
                 Container(
                   width: 300,
-                  child: Text(
-                      '${item.word} ${item.districtname ?? ''} ${item.zonename ?? ''} '),
+                  child: _title(item),
                 ),
                 Container(
+                  margin: EdgeInsets.only(top: 5),
                   width: 300,
-                  child: Text('${item.price ?? ''} ${item.type ?? ''}'),
+                  child: _subTitle(item),
                 )
               ],
             )
@@ -129,5 +152,70 @@ class _SearchPageState extends State<SearchPage> {
         ),
       ),
     );
+  }
+
+  _typeImage(String type) {
+    if (type == null) return 'images/type_travelgroup.png';
+    String path = 'travelgroup';
+    for (final val in TYPES) {
+      if (type.contains(val)) {
+        path = val;
+        break;
+      }
+    }
+    return 'images/type_$path.png';
+  }
+
+  _title(SearchItem item) {
+    if (item == null) return null;
+    List<TextSpan> spans = [];
+    spans.addAll(_keywordTextSpans(item.word, searchModel.keyword));
+    spans.add(TextSpan(
+        text: ' ' + (item.districtname ?? '') + ' ' + (item.zonename ?? ''),
+        style: TextStyle(fontSize: 16, color: Colors.grey)));
+    return RichText(
+      text: TextSpan(children: spans),
+    );
+  }
+
+  _subTitle(SearchItem item) {
+    return RichText(
+      text: TextSpan(children: <TextSpan>[
+        TextSpan(
+          text: item.price ?? '',
+          style: TextStyle(fontSize: 16, color: Colors.orange),
+        ),
+        TextSpan(
+          text: ' ' + (item.star ?? ''),
+          style: TextStyle(fontSize: 12, color: Colors.grey),
+        )
+      ]),
+    );
+  }
+
+  Iterable<TextSpan> _keywordTextSpans(String word, String keyword) {
+    List<TextSpan> spans = [];
+    if (word == null || word.length == 0) return spans;
+    //搜索关键字高亮忽略大小写
+    String wordL = word.toLowerCase(), keywordL = keyword.toLowerCase();
+    List<String> arr = wordL.split(keywordL);
+    TextStyle normalStyle = TextStyle(fontSize: 16, color: Colors.black87);
+    TextStyle keywordStyle = TextStyle(fontSize: 16, color: Colors.orange);
+    //'wordwoc'.split('w') -> [, ord, oc] @https://www.tutorialspoint.com/tpcg.php?p=wcpcUA
+    int preIndex = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (i != 0) {
+        //搜索关键字高亮忽略大小写
+        preIndex = wordL.indexOf(keywordL, preIndex);
+        spans.add(TextSpan(
+            text: word.substring(preIndex, preIndex + keyword.length),
+            style: keywordStyle));
+      }
+      String val = arr[i];
+      if (val != null && val.length > 0) {
+        spans.add(TextSpan(text: val, style: normalStyle));
+      }
+    }
+    return spans;
   }
 }
