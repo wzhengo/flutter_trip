4184c80eabde63250d14dfa32193468189340cca
wz <291171110@qq.com>
2020-01-24 16:14:16 +0800

    Flutter plugin Android注册

--- android/app/src/main/kotlin/devio/org/flutter_trip/MainActivity.kt
+++ android/app/src/main/kotlin/devio/org/flutter_trip/MainActivity.kt
@@ -1,12 +1,16 @@
 package devio.org.flutter_trip
 
 import androidx.annotation.NonNull;
+import com.wz.plugin.asr.AsrPlugin
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        val shimPluginRegistry = ShimPluginRegistry(flutterEngine)
+        AsrPlugin.registerWith(shimPluginRegistry.registrarFor("org.devio.flutter.plugin.asr.AsrPlugin"))
     }
 }
