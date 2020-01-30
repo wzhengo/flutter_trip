f612c01220b2ba6d6e28c077a53421ef10984cbc
wz <291171110@qq.com>
2020-01-30 12:17:55 +0800

    旅拍页面实现

--- lib/navigator/tab_navigator.dart
+++ lib/navigator/tab_navigator.dart
@@ -30,6 +30,7 @@ class _TabNavigatorState extends State<TabNavigator> {
           TravelPage(),
           MyPage(),
         ],
+        physics: NeverScrollableScrollPhysics(),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
